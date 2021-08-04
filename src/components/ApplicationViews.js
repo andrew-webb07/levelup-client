2651eba160ed034d41cb9b95075c044d39512c98
Andrew Webb <webba@ihopu.org>
2021-08-04 15:10:24 -0500

    created a new game form

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { GameList } from "./game/GameList.js"
 import { GameProvider } from "./game/GameProvider.js"
+import { GameForm} from "./game/GameForm.js"
 import { EventProvider } from "./event/EventProvider.js"
 import { EventList } from "./event/EventList.js"
 
@@ -18,6 +19,9 @@ export const ApplicationViews = () => {
                     </Route>
                     <Route exact path="/games">
                         <GameList />
+                    </Route>
+                    <Route exact path="/games/new">
+                        <GameForm />
                     </Route>
                     <Route exact path="/events">
                         <EventList />
